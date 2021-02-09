2a0fe1f891ec3dd16c03a242d3c9403f0d6525b0
ddikodroid <ddikodroid@gmail.com>
2021-02-09 11:07:04 +0700

    move redux provider to index.js

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import MainScreen from './src/screens/MainScreen'
-import { Provider } from 'react-redux'
-import { store } from './src/redux'
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <MainScreen />
-    </Provider>
+    <MainScreen />
   )
 }
 
