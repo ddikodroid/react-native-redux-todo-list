ec70294cc30c92cd98680a273e3d1a2cc1e55cfa
ddikodroid <ddikodroid@gmail.com>
2021-02-09 11:07:55 +0700

    delete unused code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { COLOR } from '../styles/Color'
 import { WIDTH, HEIGHT, SIZE, FONT } from '../styles/Dimension'
